3c0040687f81ba3147a7aa8eb8ad0b9cb527077d
Mohamed Ayman <mohameddallash21@gmail.com>
2023-04-10 02:18:12 -0700

    Update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,3 +1,20 @@
-int _putchar(char c){
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Prints the phrase
+ *	_putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
